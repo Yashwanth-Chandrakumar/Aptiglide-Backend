b6196a6b5feadbae4c2d37d74fad9c72e9d23c28
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-10 17:44:34 +0530

    Delete backend directory

--- backend/src/main/java/com/yashwanth/backend/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.yashwanth.backend.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Could not find the user with id:"+ id);
-    }
-}
