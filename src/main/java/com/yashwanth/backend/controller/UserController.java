6c91637acc71308ea6b5c81efe17209e45a6f386
Yashwanth S C <yashwanth2k05@gmail.com>
2023-12-10 18:37:32 +0530

    Update UserController.java

--- src/main/java/com/yashwanth/backend/controller/UserController.java
+++ src/main/java/com/yashwanth/backend/controller/UserController.java
@@ -9,7 +9,7 @@ import com.yashwanth.backend.exception.UserNotFoundException;
 import java.util.List;
 
 @RestController
-@CrossOrigin("https://aptiglide.netlify.app")
+@CrossOrigin("http://localhost:5173")
 public class UserController {
     @Autowired
     private Userrepo userrepo;
